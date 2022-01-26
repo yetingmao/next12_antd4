c1a8a1145a66996c92f60e26d541d070d37ac806
yetingmao <yetingmao@163.com>
2022-01-26 16:50:44 +0800

    删除信息

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 
-const line = "http://114.116.247.159:8038/aikg";
-const test = "http://24.59.13.170:8038/aikg";
+const line = "";
+const test = "";
 const SERVERURL = process.env.NODE_ENV === "development" ? test : line;
 
 const Global = {
